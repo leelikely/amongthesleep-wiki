40e5cd71d52ec7a24ae663e3dbe7c6f531194f03
leelikely <506995883@qq.com>
2026-05-22 21:06:25 +0800

    Add files via upload

--- amongthesleep.wiki/sw.js
+++ amongthesleep.wiki/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11031370
+    "zoneId": 11042173
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
